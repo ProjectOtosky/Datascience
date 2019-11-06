cff892ef054cdca6bda4f1484d2475f20a144619
ProjectOtosky <otoproject40@gmail.com>
2019-11-06 12:30:22 +0100

    second commit

---
+++ Training.py
@@ -0,0 +1,42 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 1+1
+2
+>>> 2*4
+8
+>>> 
+======= RESTART: C:/Users/hp/Desktop/Data Science Training/Training.py =======
+4
+>>> 
+>>> b = 2
+>>> c = 2
+>>> b =c
+>>> 
+>>> b = c
+>>> b is c
+True
+>>> b == c
+True
+>>> import this
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+>>> 
